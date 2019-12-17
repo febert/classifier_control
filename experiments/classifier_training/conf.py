4bebe1662c349f42041db313b887fe5923008e33
Suraj Nair <surajnair.caltech@gmail.com>
2019-12-17 11:04:30 -0800

    local version

--- experiments/classifier_training/conf.py
+++ experiments/classifier_training/conf.py
@@ -10,7 +10,7 @@ import imp
 configuration = {
     'model': BaseTempDistClassifier,
     'logger': TdistClassifierLogger,
-    'data_dir': os.environ['VMPC_DATA'] + '/classifier_control/data_collection/sim/1_obj_cartgripper_xz_rejsamp',       # 'directory containing data.' ,
+    'data_dir': os.environ['VMPC_DATA'] + '/classifier_control/data_collection/sim/maze_navigation',       # 'directory containing data.' ,
     'batch_size' : 32,
     # 'num_epochs': 200,
 }
