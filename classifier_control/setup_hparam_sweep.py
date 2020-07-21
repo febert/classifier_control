ee6ac47dd4954ebce72233e7365104429a99c2bf
Stephen Tian <stephentian@berkeley.edu>
2020-07-21 16:37:26 -0700

    Update BRC sweep setup script

--- classifier_control/setup_hparam_sweep.py
+++ classifier_control/setup_hparam_sweep.py
@@ -15,7 +15,7 @@ def copy_with_replace_values(file_path, new_file_name, keys, values):
                         data[i] = f'#{data[i].split(":")[0]}: default\n'
                     else:
                         data[i] = data[i].split(':')[0]
-                        data[i] += f': {value},\n'
+                        data[i] += f': {repr(value)},\n'
 
         with open(new_file_name, 'w') as w:
             w.writelines(data)
@@ -24,6 +24,7 @@ def copy_with_replace_values(file_path, new_file_name, keys, values):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("ancestral", help="path to the ancestral config file")
+    parser.add_argument("--jpb", help="jobs per batch call (to share GPUS)", type=int, default=1)
     args = parser.parse_args()
 
     conf_path = os.path.abspath(args.ancestral)
@@ -49,15 +50,15 @@ def main():
     created_files = 0
     for value_setting in itertools.product(*sweep_values):
         # value_setting is a length len(sweep_keys) iterable containing each possible hparam setting
-        setting_name = ','.join([f'{key}={str(value)}' for key, value in zip(sweep_keys, value_setting)])
+        setting_name = ','.join([f'{key}={str(value).split("/")[-1]}' for key, value in zip(sweep_keys, value_setting)])
 
         new_folder_name = os.path.join(os.path.dirname(conf_path), setting_name)
         if not os.path.exists(new_folder_name):
             os.mkdir(new_folder_name)
         new_exp_file = os.path.join(new_folder_name, 'conf.py')
         copy_with_replace_values(conf_path, new_exp_file, sweep_keys, value_setting)
-
-        run_commands.append(f'python classifier_control/train.py {new_exp_file}\n')
+        #run_commands.append(f'sbatch -A co_rail -p savio3_2080ti -t 15:00:00 -N 1 -n 1 --cpus-per-task=2 --gres=gpu:1 --qos rail_2080ti3_normal --wrap "')
+        run_commands.append(f'python classifier_control/train.py {new_exp_file}')
 
         print(f'... Create exp file for {setting_name}')
         created_files += 1
@@ -65,7 +66,16 @@ def main():
     print(f'Created {created_files} experiment directories.')
 
     with open(os.path.join(os.path.dirname(conf_path), 'batch_command.sh'), 'w') as f:
-        f.writelines(run_commands)
+        cmd_ind = 0
+        while cmd_ind < len(run_commands):
+            num_cmds = 0
+            command = f'sbatch -A co_rail -p savio3_2080ti -t 15:00:00 -N 1 -n 1 --cpus-per-task=4 --gres=gpu:1 --qos rail_2080ti3_normal --wrap "'
+            while num_cmds < args.jpb and cmd_ind < len(run_commands):
+                command += run_commands[cmd_ind] + " & "
+                cmd_ind += 1 
+                num_cmds += 1
+            command += 'wait"\n'
+            f.write(command)
 
 
 if __name__ == '__main__':
