8ae7fd5db29fdc2bb6ef9ae81d2cc9a685a760b4
Stephen Tian <stephentian@berkeley.edu>
2020-08-19 17:56:39 -0700

    Fix DistFuncEval

--- classifier_control/classifier/utils/DistFuncEvaluation.py
+++ classifier_control/classifier/utils/DistFuncEvaluation.py
@@ -4,20 +4,24 @@ import torch
 from classifier_control.baseline_costs.image_mse_cost import ImageMseCost
 from classifier_control.cem_controllers.brl_learned_cost import BRLLearnedCost
 
+
 class DistFuncEvaluation():
     def __init__(self, testmodel, testparams):
         self.models = []
+
         if testmodel is ImageMseCost:
             self.models.append(ImageMseCost())
         else:
             model_paths = testparams['classifier_restore_paths']
             model_path = testparams['classifier_restore_path']
 
-            if not model_paths:
+            if model_paths[0] == '':
                 model_paths = [model_path]
+            testparams.pop('classifier_restore_paths')
 
             for model_path in model_paths:
-                if model_path is not None and testmodel is not BRLLearnedCost:
+                testparams['classifier_restore_path'] = model_path
+                if model_path is not None:
                     config_path = '/'.join(str.split(model_path, '/')[:-2]) + '/params.yaml'
                     with open(config_path) as config:
                         overrideparams = yaml.load(config)
@@ -30,10 +34,12 @@ class DistFuncEvaluation():
                 model = testmodel(overrideparams).eval()
                 model.to(torch.device('cuda'))
                 self.models.append(model)
-                self.model = self.models[0]
+
+        self.model = self.models[0]
 
     def predict(self, inputs):
         scores = [model(inputs) for model in self.models]
         return np.max(np.stack(scores), axis=0)
+
 
 
